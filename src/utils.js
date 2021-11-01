881dfa95832be4942461e1e029151fb096af3626
Mark Cawthray <dr.cawthray@gmail.com>
2021-11-01 11:10:31 -0600

    Setup basic fetch call in utils using dotenv for key

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
 require('dotenv').config();
 
 const apiKey = process.env.REACT_APP_API_KEY;
-const appID = process.env.REACT_APP_ID;
+const url = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${query}&api-key=${apiKey}`
+
+export const getMovieReview = (query) => {
+  return fetch(url)
+    .then(response => response.json)
+}
