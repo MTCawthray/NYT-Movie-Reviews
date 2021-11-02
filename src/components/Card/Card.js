3252863bf02a1dd80f0f4304f9bf40178e708576
Mark Cawthray <dr.cawthray@gmail.com>
2021-11-02 10:29:16 -0600

    Begin to style cards that render in display area

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,24 @@
 import './Card.css';
 
-const Card = () => {
+const Card = ({ title, img, headline, link, publicationDate, summary }) => {
 
   return (
-    <article className="card">
-      <p>
-        I'm a Card!
-      </p>
+    <article className="card"  >
+      <a href={link}>
+        <h3 className="title">
+          {title}
+        </h3>
+        {img ? <img src={img.src} alt={title + ' thumbnail'} className={img.type} /> : <p>no photo available</p>}
+        <p className="headline" >
+          {headline}
+        </p>
+        <p className="summary" >
+          {summary}
+        </p>
+        <p className="published-date">
+          published on {publicationDate}
+        </p>
+      </a>
     </article>
   )
 }
--- src/components/DisplayArea/DisplayArea.js
+++ src/components/DisplayArea/DisplayArea.js
@@ -1,11 +1,27 @@
 import './DisplayArea.css';
-import '../Card/Card';
+import Card from '../Card/Card';
 
-const DisplayArea = () => {
+const DisplayArea = ({ results }) => {
+
+  const makeCards = results.map(result => {
+      console.log('img url test in display area', result.multimedia)
+      return (
+        <Card 
+          title={result.displayTitle}
+          img={result.multimedia}
+          headline={result.headline}
+          link={result.link}
+          publicationDate={result.publication_date}
+          sumary={result.summary_short}
+          key={result.date_updated}
+        />
+      )
+
+  });
 
   return (
-    <section className="displayArea">
-      <h2>I'm the display area!</h2>
+    <section className="display-area">
+      {makeCards}
     </section>
   )
 }
