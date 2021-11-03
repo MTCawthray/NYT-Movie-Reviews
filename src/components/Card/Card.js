466673b1f5766fbf69d7a98b918a5aae1ea0a920
Mark Cawthray <dr.cawthray@gmail.com>
2021-11-03 12:42:36 -0600

    Format return for card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,24 +1,40 @@
 import './Card.css';
 
-const Card = ({ title, img, headline, link, publicationDate, summary }) => {
+const Card = ({ selectedReview }) => {
+
+  const { byline, critics_pick, date_updated, display_title, headline, link, mpaa_rating, multimedia, opening_date, publication_date, summary_short } = selectedReview
 
   return (
     <article className="card"  >
-      <a href={link}>
-        <h3 className="title">
-          {title}
-        </h3>
-        {img && <img src={img.src} alt={title + ' thumbnail'} className={img.type} />}
-        <p className="headline" >
-          {headline}
-        </p>
-        <p className="summary" >
-          {summary}
-        </p>
-        <p className="published-date">
-          published on {publicationDate}
-        </p>
+    <div className="title-rating-holder">
+      <h2 className="title-info">
+        {display_title}
+      </h2>
+      <p>
+        Rated: {mpaa_rating} | 
+      </p>
+      <p>
+        opened: {opening_date}
+      </p>
+    </div>
+    {multimedia && <img src={multimedia.src} alt={display_title} className={multimedia.type} />}
+    <h3 className="headline">
+      {headline} written by {byline}
+    </h3>
+    <h3 className="movie-italic">
+      published on {publication_date}. updated on {date_updated}.
+    </h3> 
+    <div className="link-holder">
+      <a href={link.url} >
+        {link.suggested_link_text}
       </a>
+    </div>
+
+    <p className="summary-holder">
+      <strong>Summary: </strong>
+      {summary_short}
+    </p>
+    
     </article>
   )
 }
