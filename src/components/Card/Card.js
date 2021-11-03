040d6c2e57b7eed607606efe6feeaac3240abd30
Mark Cawthray <dr.cawthray@gmail.com>
2021-11-03 15:53:29 -0600

    Style components to render nicely

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,36 +5,36 @@ const Card = ({ selectedReview }) => {
   const { byline, critics_pick, date_updated, display_title, headline, link, mpaa_rating, multimedia, opening_date, publication_date, summary_short } = selectedReview
 
   return (
-    <article className="card"  >
-    <div className="title-rating-holder">
-      <h2 className="title-info">
-        {display_title}
-      </h2>
-      <p>
-        Rated: {mpaa_rating} | 
+    <article className="card"> 
+      <div className="title-rating-holder">
+        <h2 className="title-info">
+          {display_title}
+        </h2>
+        <p>
+          Rated: {mpaa_rating} | 
+        </p>
+        <p>
+          opened: {opening_date}
+        </p>
+      </div>
+      {multimedia && <img src={multimedia.src} alt={display_title} className={multimedia.type} />}
+      <h3 className="headline">
+        {headline} written by {byline}
+      </h3>
+      <p className="summary-holder">
+        <strong>Summary: </strong>
+        {summary_short}
       </p>
-      <p>
-        opened: {opening_date}
-      </p>
-    </div>
-    {multimedia && <img src={multimedia.src} alt={display_title} className={multimedia.type} />}
-    <h3 className="headline">
-      {headline} written by {byline}
-    </h3>
-    <p className="summary-holder">
-      <strong>Summary: </strong>
-      {summary_short}
-    </p>
-    <h3 className="movie-italic">
-      published on {publication_date}. updated on {date_updated}.
-    </h3> 
-    <div className="link-holder">
-      <a href={link.url} >
-        {link.suggested_link_text}
-      </a>
-    </div>
-    
-    </article>
+      <h3 className="movie-italic">
+        published on {publication_date}. updated on {date_updated}.
+      </h3> 
+      <div className="link-holder">
+        <a href={link.url} >
+          {link.suggested_link_text}
+        </a>
+      </div>
+    </article> 
+  
   )
 }
 
