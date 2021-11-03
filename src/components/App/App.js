6f4a58ee8b2cd6697e618836a5ff8707147493b2
Mark Cawthray <dr.cawthray@gmail.com>
2021-11-03 11:16:51 -0600

    Create select review method to pass to display area

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,28 @@ import { useState } from 'react';
 import { getMovieReview } from '../../utils.js';
 import Header from '../Header/Header';
 import DisplayArea from '../DisplayArea/DisplayArea';
+import Card from '../Card/Card';
 
 function App() {
 
   const [reviews, setReviews] = useState([])
+  const [selectedReview, setSelectedReview] = useState(null)
+
+  const selectReview = (target) => {
+    console.log(target)
+    setSelectedReview(null);
+
+    reviews.find(review => {
+      // console.log(review.headline, target)
+      if (target.id === review.headline) {
+        console.log('match!')
+        setSelectedReview(review);
+      }
+      console.log('did not match')
+      return
+    })
+
+  }
 
   const findReviews = (searchQuery) => {
     getMovieReview(searchQuery)
@@ -20,8 +38,14 @@ function App() {
         findReviews={findReviews}
       />
       <section className="content-holder">
-        <DisplayArea results={reviews} />
-
+        <DisplayArea 
+          results={reviews} 
+          select={selectReview}
+        />
+        {selectedReview && (
+        <Card
+          selectedReview={selectedReview}
+        />)}
       </section>
     </div>
   );
--- src/components/DisplayArea/DisplayArea.js
+++ src/components/DisplayArea/DisplayArea.js
@@ -1,23 +1,18 @@
 import './DisplayArea.css';
-import Card from '../Card/Card';
 
-const DisplayArea = ({ results }) => {
+const DisplayArea = ({ results, select }) => {
 
   const makeCards = results.map(result => {
       console.log('img url test in display area', result)
       return (
-        // <Card 
-        //   headline={result.headline}
-        //   publicationDate={result.publication_date}
-        //   key={result.date_updated}
-        // />
-
         <li 
         className="review-snippet"
-        key={result.date_updated}>
+        key={result.date_updated}
+        id={result.headline}
+        onClick={(e) => select(e.target)}>
           <div className="snippet-holder">
-            <p>
-              <strong>{result.headline}</strong> <em>published {result.publication_date}</em>
+            <p id={result.headline}>
+              {result.headline} published {result.publication_date}
             </p>
           </div>
         </li>
