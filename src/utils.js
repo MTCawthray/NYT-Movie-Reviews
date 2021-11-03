ff5c1d3a1cda6d70ddea838af5c6f2aae2a42795
Mark Cawthray <dr.cawthray@gmail.com>
2021-11-03 14:17:37 -0600

    Create fetch method to get recent critics picks

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 
 const apiKey = process.env.REACT_APP_API_KEY;
-// const url = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${query}&api-key=${apiKey}`
 
 export const getMovieReview = (searchQuery) => {
   return fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${searchQuery}&api-key=${apiKey}`)
+    .then(response => response.json())
+}
+
+export const getCriticsPicks = () => {
+  return fetch(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${apiKey}`)
     .then(response => response.json())
 }
